0fd881d6f751d10baa5b4c18a3b1d90a91d67482
Xavier <xavier@XavierdeMacBook-Pro.local>
2016-02-29 20:09:01 +0800

    Initial Commit

---
+++ MoodDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoodDiary
+//
+//  Created by Xavier on 16-2-29.
+//  Copyright (c) 2016年 Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoodDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoodDiary
+//
+//  Created by Xavier on 16-2-29.
+//  Copyright (c) 2016年 Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
